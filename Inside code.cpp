2f0d002a9d297bbcc3efa4a8715de4f25100d844
Roza895 <roza294165@gmail.com>
2025-02-23 21:35:58 +0600

    Add files via upload

---
+++ Inside code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class state
+{
+public:
+    void cse()
+    {
+        cout<<"welcome to cse"<<endl;
+    }
+};
+int main ()
+{
+    state obj;
+    obj.cse();
+    return 0;
+}
