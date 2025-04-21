8f494d9f554cc66d7fc1773eb710680bef741171
Roza895 <roza294165@gmail.com>
2025-04-21 13:29:05 +0600

    Add files via upload

---
+++ Hierarchical In.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class vehicle
+{
+public:
+    vehicle()
+    {
+        cout << "This is a vehicle" << endl;
+    }
+};
+class car: public vehicle
+
+{
+
+};
+class bus: public vehicle
+{
+
+};
+int main()
+{
+    car obj1;
+    bus obj2;
+    return 0;
+}
